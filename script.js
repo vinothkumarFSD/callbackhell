6c515410f786c33cc8ff8dc04dd2961d7e43e5f9
vinothkumarFSD <125821734+vinothkumarFSD@users.noreply.github.com>
2023-05-09 23:28:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+setTimeout(() => {
+    document.getElementById('countdown').innerHTML = 10;
+    setTimeout(() => {
+        document.getElementById('countdown').innerHTML = 9;
+        setTimeout(() => {
+            document.getElementById('countdown').innerHTML = 8;
+            setTimeout(() => {
+                document.getElementById('countdown').innerHTML = 7;
+                setTimeout(() => {
+                    document.getElementById('countdown').innerHTML = 6;
+                    setTimeout(() => {
+                        document.getElementById('countdown').innerHTML = 5;
+                        setTimeout(() => {
+                            document.getElementById('countdown').innerHTML = 4;
+                            setTimeout(() => {
+                                document.getElementById('countdown').innerHTML = 3;
+                                setTimeout(() => {
+                                    document.getElementById('countdown').innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.getElementById('countdown').innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.getElementById('countdown').innerHTML = "Happy Independance Day";
+                                        }, 1000);
+
+                                    }, 1000);
+
+                                }, 1000);
+
+                            }, 1000);
+
+                        }, 1000);
+
+                    }, 1000);
+
+                }, 1000);
+
+            }, 1000);
+
+        }, 1000);
+
+    }, 1000);
+
+}, 1000);
+
+var countdown=document.createElement("h1");
+countdown.style.color="red";
+countdown.style.textAlign="center";
+countdown.style.marginTop="100px";
+countdown.setAttribute("id","countdown");
+document.body.append(countdown);
